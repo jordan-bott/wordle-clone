bf696f12052a249420f83548d75b5e07b6f67ffe
jordan-bott <jordanbott.dev@gmail.com>
2024-03-09 14:20:59 -0800

    additional features for schema - builds final schema, gives more information about turn number, reveals word at the end

--- main.py
+++ main.py
@@ -7,29 +7,32 @@ def main(wordle_word):
 
     wordle_word = wordle_word.upper()
     guessed_words = []
+    final_schema = ""
 
     for i in range(1, 7):
         guess = input(f"Enter wordle guess number {i}:\t").upper()
-
-        if guess == wordle_word:
-            return print("You won the game!")
-
         valid_word = check_word_validity(guess, guessed_words)
 
         if not valid_word:
             is_valid = False
             while is_valid is False:
                 guess = input(f"Try guess number {i} again:\t").upper()
-                if guess == wordle_word:
-                    return print("You won the game!")
                 valid_word = check_word_validity(guess, guessed_words)
                 if valid_word:
                     is_valid = True
         guessed_words.append(guess)
         wordle_schema = create_schema(wordle_word, guess)
-        print(wordle_schema)
+        final_schema += f"{wordle_schema}\n"
 
-    print("Sorry, you didn't win this time")
+        if guess == wordle_word:
+            print(f"You won in {i} turn{'s' if i != 1 else ''}! Great job")
+            print(final_schema)
+            return None
+        else:
+            print(wordle_schema)
+
+    print(f"Sorry, you didn't win this time\nThe word was: {wordle_word.lower()}.\n")
+    print(final_schema)
 
 
 main(sys.argv[1])
